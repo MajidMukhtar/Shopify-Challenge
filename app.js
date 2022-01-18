1ecc86d9fb0182effcff9a217f5936a0a5849e02
Majid Mukhtar <majidmukhtar837@gmail.com>
2022-01-18 12:38:04 -0500

    Working next button

--- app.js
+++ app.js
@@ -21,12 +21,13 @@ function fetchData1() {
 
 fetchData1()
 
-// Get a random Mars Picture from Nasa API
+// Get a random Mars Picture from rover, Curiosisty
 
 function fetchData2() {
     fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=mkjbyQXrVc6Ku8oI2Pl6G640WBWuhdJLxt8vBSWP").then(response => {// data about the call
         return response.json()// shows the data from the call as a JavaScript object
     }).then(data => {// the data inside the response
+        let photos = data.photos
         let photoInfo = data.photos[10]
         let roverInfo = photoInfo.rover
 
@@ -38,40 +39,71 @@ function fetchData2() {
         let landDate = roverInfo.landing_date
         let roverStatus = roverInfo.status
         
-        if($("#nextBtn").onclick = true) {
-            photoInfo[ + 1]
-        }
-
+      
 
         $("#pic2").attr("src", image)
         $("#title2").html("Rover Name: " + roverName)
         $("#date2").html(date)
         $("#text2").html(roverName + " was launched on " + launchDate + " and made landfall on Mars on " + landDate + "." + " It's status is currently " + roverStatus + ".")
     
+
+    // $("#nextBtn").onclick = function() {
+    //     console.log()
+    // }
+
+console.log(data.photos[0 + 2])
+
     
+        document.querySelector("#nextBtn").onclick = function() {
+            for(let i = 0; i < photos.length; i++) {
+                let nextPhoto = data.photos[i + 1]
+                let img = nextPhoto.img_src
+                $("#pic2").attr("src", img)
+
+            }
+        }
+
+
     }).catch(error => {// if the call doesn't work, it will console log the error message
         console.log(error)
     })
 }
 
+
+
+
 fetchData2()
 
+// Get a random Mars Picture from rover, Spirit
+
+
 function fetchData3() {
-    fetch("https://api.nasa.gov/planetary/earth/assets").then(response => {// data about the call
+    fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=1000&api_key=mkjbyQXrVc6Ku8oI2Pl6G640WBWuhdJLxt8vBSWP").then(response => {// data about the call
         return response.json()// shows the data from the call as a JavaScript object
     }).then(data => {// the data inside the response
-        console.log(data)
+       
+        let photoInfo = data.photos[0]
+        let roverInfo = photoInfo.rover
 
-      
-        // let date = data.date
-        // let title = data.title
-        // let image = data.hdurl
-        // let descrip = data.explanation 
 
-        // $("#pic1").attr("src", image)
-        // $("#title1").html(title)
-        // $("#text1").html(descrip)
-        // $("#date1").html(date)
+        let image = photoInfo.img_src
+        let roverName = roverInfo.name
+        let date = photoInfo.earth_date
+        let launchDate = roverInfo.launch_date
+        let landDate = roverInfo.landing_date
+        let roverStatus = roverInfo.status
+        
+        // if($("#nextBtn").onclick = true) {
+        //     photoInfo[ + 1]
+        // }
+
+
+        $("#pic3").attr("src", image)
+        $("#title3").html("Rover Name: " + roverName)
+        $("#date3").html(date)
+        $("#text3").html(roverName + " was launched on " + launchDate + " and made landfall on Mars on " + landDate + "." + " It's status is currently " + roverStatus + ".")
+
+
     }).catch(error => {// if the call doesn't work, it will console log the error message
         console.log(error)
     })
@@ -109,3 +141,4 @@ function like(text) {
     }
 }
 
+// api_key=mkjbyQXrVc6Ku8oI2Pl6G640WBWuhdJLxt8vBSWP
